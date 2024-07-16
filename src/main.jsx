8973f6f9476ff89e58f709f6fecd4d0dcfe7e627
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-07-17 01:22:57 +0600

    login form completed

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import RegistrationForm from './components/Registration'
+import LoginForm from './components/Login'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RegistrationForm/>
+    {/* <RegistrationForm/> */}
+    <LoginForm/>
   </React.StrictMode>,
 )
