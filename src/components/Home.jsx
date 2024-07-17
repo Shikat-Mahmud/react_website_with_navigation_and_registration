8e8deb35cb1ddd22c1504601f79212016b9fe851
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2024-07-17 19:58:00 +0600

    home page and navigation bar added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,13 @@
 const Home = () => {
     return (
         <>
-            <section>
-                <div className="container">
-                    <h1>
-                        Hey, this is home.
-                    </h1>
-                </div>
-            </section>
+    <section class="bg-white">
+        <div class="container mx-auto px-6 py-20 text-center">
+            <h1 class="text-5xl font-bold text-gray-800 mb-4">Welcome to MyWebsite</h1>
+            <p class="text-gray-600 mb-8">This is a simple hero section to showcase the main message of your website.</p>
+            <a href="https://eftakharmahmud.com" class="bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600">Get Started</a>
+        </div>
+    </section>
         </>
     );
 };
